822d2531cd48614c2a2400788aa0aeeae70c66f7
Vitsentovich <vitsentovich.utd@gmail.com>
2017-09-14 17:29:53 +0300

    add tables

--- exercise_5/5.1.php
+++ exercise_5/5.1.php
@@ -16,11 +16,11 @@ $tables = [];
 
 $tables[] = "CREATE TABLE Products (
 product_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-product_size VARCHAR(30) NOT NULL,
+product_color_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+product_size_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+product_category_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 product_count INT UNSIGNED NOT NULL,
-product_color VARCHAR(50),
-product_price DECIMAL(5, 2) NOT NULL,
-product_category VARCHAR(255) NOT NULL
+product_price DECIMAL(5, 2) NOT NULL
 )";
 
 $tables[] = "CREATE TABLE Customers (
@@ -34,6 +34,36 @@ $tables[] = "CREATE TABLE Orders (
 customer_id INT UNSIGNED PRIMARY KEY,
 product_id INT UNSIGNED PRIMARY KEY,
 product_count INT UNSIGNED NOT NULL
+)";
+
+$tables[] = "CREATE TABLE Product_Color (
+color_id INT UNSIGNED PRIMARY KEY,
+product_color_id INT UNSIGNED PRIMARY KEY
+)";
+
+$tables[] = "CREATE TABLE Colors (
+color_id INT UNSIGNED PRIMARY KEY,
+color VARCHAR(30) NOT NULL
+)";
+
+$tables[] = "CREATE TABLE Product_Size (
+size_id INT UNSIGNED PRIMARY KEY,
+product_size_id INT UNSIGNED PRIMARY KEY
+)";
+
+$tables[] = "CREATE TABLE Sizes (
+size_id INT UNSIGNED PRIMARY KEY,
+size VARCHAR(30) NOT NULL
+)";
+
+$tables[] = "CREATE TABLE Product_Category (
+category_id INT UNSIGNED PRIMARY KEY,
+product_category_id INT UNSIGNED PRIMARY KEY
+)";
+
+$tables[] = "CREATE TABLE Sizes (
+category_id INT UNSIGNED PRIMARY KEY,
+category VARCHAR(30) NOT NULL
 )";
 
 fillDB($tables, $connection);
